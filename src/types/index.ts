acbe39dadad0462f9b975149a112fd9d9c13e52f
wenfengwei <wenfwcn@163.com>
2020-04-27 17:45:01 +0800

    重载

--- src/types/index.ts
+++ src/types/index.ts
@@ -58,4 +58,5 @@ export interface Axios {
 
 export interface AxiosInstance extends Axios {
   (config: AxiosRequestConfig): AxiosPromise
+  (url: string, config?: AxiosRequestConfig): AxiosInstance
 }
