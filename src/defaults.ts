6d81cecfcae3c216e48586510edd108826ec6fbc
wenfengwei <wenfwcn@163.com>
2020-08-26 09:47:18 +0800

    transform 默认逻辑

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from './types'
 import { processHeaders } from './helpers/headers'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 
 const defaults: AxiosRequestConfig = {
   method: 'get',
@@ -16,7 +16,11 @@ const defaults: AxiosRequestConfig = {
       return transformRequest(data)
     }
   ],
-  transformResponse: []
+  transformResponse: [
+    function(data: any): any {
+      return transformResponse(data)
+    }
+  ]
 }
 
 const methodsNoData = ['delete', 'get', 'head', 'options']
